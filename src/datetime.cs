da644cbc38ce8ea7149073b89bbf6d2deae7972c
unknown <yosin@yosin3-w.ad.corp.google.com>
2014-07-17 11:27:20 +0900

    Introduce datetime.cs

---
+++ src/datetime.cs
@@ -0,0 +1,8 @@
+using System;
+
+class PrintDateTime {
+public static void Main() {
+  // See also http://msdn.microsoft.com/en-us/library/8kb3ddd4(v=vs.110).aspx
+  Console.WriteLine("{0:s}", DateTime.Now);
+}
+}
