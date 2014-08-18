25ddab6d1f269aa146b03db84a1cc07ffc92da31
Eval <eval1749@gmail.com>
2014-08-18 18:39:01 +0900

    [editing] Introduce |editing.library.isVisibleNode|.

--- src/editing/editing_library.js
+++ src/editing/editing_library.js
@@ -9,14 +9,61 @@ editing.define('library', (function() {
    * @param {!EditingNode} node
    * @return {boolean}
    */
+  function isVisibleNode(node) {
+    console.assert(node instanceof editing.EditingNode);
+    if (isWhitespaceNode(node))
+      return false;
+    if (node.domNode.nodeType != Node.ELEMENT_NODE)
+      return isVisibleNode(node.parentNode);
+    var style = window.getComputedStyle(node.domNode);
+    if (style)
+      return style.display != 'none';
+    if (!node.parentNode)
+      return node.nodeType == Node.TEXT_NODE;
+    return isVisibleNode(node.parentNode);
+  }
+
+  /**
+   * @param {!EditingNode} node
+   * @return {boolean}
+   */
   function isWhitespaceNode(node) {
+    console.assert(node instanceof editing.EditingNode);
     if (!node.isText)
       return false;
     var text = node.nodeValue.replace(/[ \t\r\n]/g, '');
     return text == '';
   }
 
+
+  // nextNode(<a><b>foo|</b><a>bar) = bar
+  function nextNode(current) {
+    if (current.firstChild)
+      return current.firstChild;
+    if (current.nextSibling)
+      return current.nextSibling;
+    return nextAncestorOrSibling(current);
+  }
+
+  function nextAncestorOrSibling(current) {
+    console.assert(!current.nextSibling);
+    for (var parent = current.parentNode; parent; parent = parent.parentNode) {
+      if (parent.nextSibling)
+        return parent.nextSibling;
+    }
+    return null;
+  }
+
+  function nextNodeSkippingChildren(current) {
+    if (current.nextSibling)
+      return current.nextSibling;
+    return nextAncestorOrSibling(current);
+  }
+
   return Object.defineProperties({}, {
-    isWhitespaceNode: {value: isWhitespaceNode}
+    isVisibleNode: {value: isVisibleNode},
+    isWhitespaceNode: {value: isWhitespaceNode},
+    nextNode: {value: nextNode},
+    nextNodeSkippingChildren: {value: nextNodeSkippingChildren},
   });
 })());
