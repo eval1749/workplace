862e81f61e1f8d7dde1c3e25f95062fe0b60c6a1
Eval <eval1749@gmail.com>
2014-08-19 00:13:18 +0900

    [edigint] polyfill: Introduce |Array.prototype.findIndex| for |getEffectiveNodes|.

--- src/editing/polyfill.js
+++ src/editing/polyfill.js
@@ -12,6 +12,36 @@
 // - |window.getComputedStyle| throws an exception for Node.TEXT_NODE, Chrome
 //   returns null.
 
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function(predicate) {
+      if (this == null) {
+        throw new TypeError('Array.prototype.find called on null or undefined');
+      }
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+      var list = Object(this);
+      var length = list.length >>> 0;
+      var thisArg = arguments[1];
+      var value;
+
+      for (var i = 0; i < length; i++) {
+        if (i in list) {
+          value = list[i];
+          if (predicate.call(thisArg, value, i, list)) {
+            return i;
+          }
+        }
+      }
+      return -1;
+    }
+  });
+}
+
 if (!Math.sign) {
   Math.sign = function(x) {
     if (isNaN(x))
