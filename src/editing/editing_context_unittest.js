12384ef38d0faad0eaa582c697ed9a28eb2f4a33
Eval <eval1749@gmail.com>
2014-08-23 19:08:30 +0900

    [editing] testing: Move |EditingContext.prototype.split{Node,Text}| tests to "editing_context_unittest.js".

---
+++ src/editing/editing_context_unittest.js
@@ -0,0 +1,76 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function dumpNodes(nodes) {
+  var sink = ''
+  var delimiter = '';
+  nodes.forEach(function(node) {
+    if (editing.nodes.isText(node))
+      sink += delimiter + node.nodeValue;
+    else
+      sink += delimiter + node.nodeName;
+    delimiter = ',';
+  });
+  return sink;
+}
+//
+// splitNode
+//
+testCaseWithSample('context.splitNode.1',
+    '<p contenteditable><a>one|<b>two</b>three</a></p>',
+    function(context, selectionIn) {
+      var oldTree = context.document.querySelector('a');
+      var refNode = oldTree.childNodes[1];
+      var newTree = context.splitNode(oldTree, refNode);
+      expectEq('<a>one</a>',
+               function() { return testing.serialzieNode(oldTree); });
+      expectEq('<a><b>two</b>three</a>',
+              function() { return testing.serialzieNode(newTree); });
+      expectEq(newTree, function() { return oldTree.nextSibling; });
+    });
+
+// We don't copy "id" attribute.
+testCaseWithSample('context.splitNode.2',
+    '<p contenteditable><a id="foo">one|<b>two</b>three</a></p>',
+    function(context, selectionIn) {
+      var oldTree = context.document.querySelector('a');
+      var refNode = oldTree.childNodes[1];
+      var newTree = context.splitNode(oldTree, refNode);
+      expectEq('<a id="foo">one</a>',
+               function() { return testing.serialzieNode(oldTree); });
+      expectEq('<a><b>two</b>three</a>',
+              function() { return testing.serialzieNode(newTree); });
+      expectEq(newTree, function() { return oldTree.nextSibling; });
+    });
+
+//
+// splitTree
+//
+testCaseWithSample('context.splitTree.Shallow',
+    '<p contenteditable><e1>one</e1>|<e2>two</e2><e3>three</e3></p>',
+    function(context, selectionIn) {
+      var selection = editing.nodes.normalizeSelection(context, selectionIn);
+      var refNode = selection.focusNode.childNodes[selection.focusOffset];
+      var oldTree = refNode.parentNode;
+      var newTree = context.splitTree(oldTree, refNode);
+      expectEq('<p contenteditable><e1>one</e1></p>',
+               function() { return testing.serialzieNode(oldTree); });
+      expectEq('<p contenteditable><e2>two</e2><e3>three</e3></p>',
+              function() { return testing.serialzieNode(newTree); });
+    });
+
+testCaseWithSample('context.splitTree.Deep',
+    '<p contenteditable><b>bold1<i>italic1<s>strike1|strike2</s>italic2</i>bold2</b></p>',
+    function(context, selectionIn) {
+      var selection = editing.nodes.normalizeSelection(context, selectionIn);
+      var refNode = selection.focusNode.childNodes[selection.focusOffset];
+      var oldTree = refNode.parentNode.parentNode.parentNode;
+      var newTree = context.splitTree(oldTree, refNode);
+      expectEq('<b>bold1<i>italic1<s>strike1</s></i></b>',
+               function() { return testing.serialzieNode(oldTree); });
+      expectEq('<b><i><s>strike2</s>italic2</i>bold2</b>',
+               function() { return testing.serialzieNode(newTree); });
+    });
